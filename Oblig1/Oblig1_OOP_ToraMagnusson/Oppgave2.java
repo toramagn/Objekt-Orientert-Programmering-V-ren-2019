8e673a5cb9a50e10dd4fd4d6f95c4f46f2aacacd
toramagn <43746346+toramagn@users.noreply.github.com>
2019-03-19 13:13:57 +0100

    Add files via upload

---
+++ Oblig1/Oblig1_OOP_ToraMagnusson/Oppgave2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Oppgave2 { /*endret filnavnet fra "Oppgave.java" til "Oppgave2.java", 
+                        da maatte jeg ogsaa endre "public class Oppgave" til "public class Oppgave2" */
+
+    public static void main(String[] args) {
+        // Oppretter og instansierer et objekt av typen Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Printer ut til konsollen
+        System.out.println("Skriv inn fornavn: ");
+        // Leser input fra konsollen
+        String fornavn = scanner.nextLine();
+
+
+        // Printer ut til konsollen
+        System.out.println("Skriv inn etternavn: ");
+        // Leser input fra konsollen
+        String etternavn = scanner.nextLine();
+
+        // Printer ut til konsollen
+        System.out.println("Skriv inn alder: ");
+        // Leser input fra konsollen
+        int alder = scanner.nextInt(); //bruker int her siden alder er et tall.
+
+        System.out.println("\nDitt navn er: " + fornavn + " " + etternavn + "\nDin alder er: " + alder);
+    }
+
+}
---
+++ Oblig1/Oblig1_OOP_ToraMagnusson/Oppgave3Main.java
@@ -0,0 +1,11 @@
+public class Oppgave3Main{
+
+    public static void main(String[] args) {
+    
+        Oppgave3 person1 = new Oppgave3("Linus","Torvald",39);
+
+        person1.skrivUtElevInfo();
+        //System.out.println(skrivUtElevinfo());
+    }
+
+}
